e99562228268c5eac08a9d1a0fb995fb1abd9b70
Glaubert01 <glaubert@outlook.com>
2025-08-29 20:30:08 -0300

    feat(post): criado o metodo post da API

--- js/api.js
+++ js/api.js
@@ -8,7 +8,25 @@ const api = {
             alert('Não foi possível conectar com a API');
             throw new Error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } 
+        catch  {
+            alert('Não foi possível conectar com a API');
+            throw new Error
+        }
     }
+
 }
 
 export default api;
