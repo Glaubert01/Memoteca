2e044ab46c81fdd08275ed6ef7bb72c3af05f62e
Glaubert01 <glaubert@outlook.com>
2025-08-29 22:08:57 -0300

    feat(cancelar): criada a programacao do botao cancelar

--- js/api.js
+++ js/api.js
@@ -1,32 +1,32 @@
+
 const api = {
-    async buscarPensamentos() {
-        try {
-            const response = await fetch('http://localhost:3000/pensamentos');
-            return await response.json();
-        } 
-        catch  {
-            alert('Não foi possível conectar com a API');
-            throw new Error
-        }
-    },
-
-    async salvarPensamento(pensamento) {
-        try {
-            const response = await fetch('http://localhost:3000/pensamentos',{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(pensamento)
-            });
-            return await response.json();
-        } 
-        catch  {
-            alert('Não foi possível conectar com a API');
-            throw new Error
-        }
+  async buscarPensamentos() {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos')
+      return await response.json()
     }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
 
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  }
 }
 
-export default api;
+export default api
